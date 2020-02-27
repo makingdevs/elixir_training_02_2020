3311982c8b3a89d7e6e13aec2aa3169bb483468d
neodevelop <juan@makingdevs.com>
2020-02-27 16:51:27 -0600

    Change greet function

--- lib/elixir_essential_practice/hello.ex
+++ lib/elixir_essential_practice/hello.ex
@@ -2,7 +2,7 @@ defmodule ElixirEssentialsPractice.Hello do
   def greet(), do: say() <> " You !!!"
   def greet(name), do: "Hello #{name} !!!"
   def greet(name1, name2), do: "Hello #{name1} and #{name2} !!!"
-  defp say(), do: "Alo"
+  defp say(), do: "howdy"
 
   def display([]), do: IO.puts "Empty"
   def display([h | []]), do: IO.puts "Solo uno #{h}"
