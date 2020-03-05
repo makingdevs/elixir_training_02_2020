3d0345b92b2c482e47710a2c0bfde43de125c9b5
neodevelop <juan@makingdevs.com>
2020-03-05 13:36:35 -0600

    Solution for AoC Day 3/1 2015

--- lib/elixir_essential_practice/advent_of_code.ex
+++ lib/elixir_essential_practice/advent_of_code.ex
@@ -17,5 +17,27 @@ defmodule ElixirEssentialsPractice.AdventOfCode do
   defp execute_instruction(")"), do: -1
   defp execute_instruction(_),   do:  0
 
+  def santa_deliver_gifts_in_grid(instructions) do
+    instructions
+    |> String.split("")
+    |> follow_instructions_in_grid([{0,0}], {0,0})
+  end
+
+  defp follow_instructions_in_grid([], grid, _current), do: grid
+  defp follow_instructions_in_grid([h|t], grid, current) do
+    movement = evaluate_the_instruction(h)
+    new_location = move_santa(current, movement)
+    new_grid = grid ++ [new_location]
+    follow_instructions_in_grid(t, new_grid, new_location)
+  end
+
+  defp evaluate_the_instruction("^"), do: {0, 1}
+  defp evaluate_the_instruction("<"), do: {-1, 0}
+  defp evaluate_the_instruction(">"), do: {1, 0}
+  defp evaluate_the_instruction("v"), do: {0, -1}
+  defp evaluate_the_instruction(_), do: {0, 0}
+
+  defp move_santa({x1,y1}, {x2, y2}), do: {x1+x2, y1+y2}
+
 end
 
