85f9a3fe7d055a33c5d3cc043c1938aefb9509eb
neodevelop <juan@makingdevs.com>
2020-03-05 13:16:17 -0600

    Tail recursive

--- lib/elixir_essential_practice/factorial.ex
+++ lib/elixir_essential_practice/factorial.ex
@@ -1,4 +1,8 @@
 defmodule ElixirEssentialsPractice.Factorial do
   def of(1), do: 1
   def of(n), do: n * of(n-1)
+
+  def of_no_stack(n), do: of_no_stack(n, 1)
+  def of_no_stack(1, state), do: state
+  def of_no_stack(n, state), do: of_no_stack(n-1, state * n)
 end
