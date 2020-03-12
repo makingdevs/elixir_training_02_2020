a279fa73490e2a675094761c63ccbe200654526c
neodevelop <juan@makingdevs.com>
2020-03-12 13:31:30 -0600

    Add control in process

--- lib/elixir_essential_practice/worker.ex
+++ lib/elixir_essential_practice/worker.ex
@@ -2,9 +2,14 @@ defmodule ElixirEssentialsPractice.Worker do
 
   def job(a,b) do
     receive do
-      :display -> IO.puts("#{a} - #{b}")
-      _ -> IO.puts("bye")
+      :display ->
+        IO.puts("#{a} - #{b}")
+        job(a,b)
+      :die ->
+          IO.puts("bye")
+        _ ->
+          IO.puts("invalid")
+          job(a,b)
     end
-    job(a,b)
   end
 end
