15d23c019d6f64a1ecea76e8ea2f2402879dba18
neodevelop <juan@makingdevs.com>
2020-03-19 16:51:24 -0600

    Add the factorial inside a process

--- lib/elixir_essential_practice/worker.ex
+++ lib/elixir_essential_practice/worker.ex
@@ -2,6 +2,23 @@ defmodule ElixirEssentialsPractice.Worker do
 
   alias ElixirEssentialsPractice.Factorial
 
+  def factorial_in_process(n) do
+    spawn_process(self(), n)
+    |> await()
+  end
+
+  defp spawn_process(caller, n) do
+    spawn fn ->
+      send caller, {self(), :result, Factorial.of(n)}
+    end
+  end
+
+  def await(pid) do
+    receive do
+      {^pid, :result, result} -> result
+    end
+  end
+
   def job(a,b) do
     receive do
       {:compute, caller, n} ->
@@ -11,6 +28,8 @@ defmodule ElixirEssentialsPractice.Worker do
       :status ->
         IO.puts "#{a} times"
         job(a,b)
+      :error ->
+        raise "Pelas"
       :die ->
           IO.puts("bye")
         _ ->
