08700217961254ce630624caeb7cbd5e48c46b0b
neodevelop <juan@makingdevs.com>
2020-03-25 14:59:59 -0600

    Showing the process chain

--- lib/elixir_essential_practice/process_chain.ex
+++ lib/elixir_essential_practice/process_chain.ex
@@ -2,22 +2,22 @@ defmodule ElixirEssentialsPractice.ProcessChain do
 
   def chain do
     receive do
-      {:continue, 0} ->
-        IO.puts "Process 0 Stop"
+      {:continue, 0, caller} ->
+        IO.puts "Process 0 in #{inspect self()} from #{inspect caller}"
         :stop
-      {:continue, n} ->
-        IO.puts "Process #{n} in #{inspect self()}"
+      {:continue, n, caller} ->
+        IO.puts "Process #{n} in #{inspect self()} from #{inspect caller}"
         :timer.sleep 1000
         pid = spawn_link(__MODULE__, :chain, [])
-        send pid, {:continue, n - 1}
+        send pid, {:continue, n - 1, self()}
       _ -> :stop
     end
     chain()
   end
 
-  def start() do
+  def start(caller, chain_size \\ 10) when is_pid(caller) do
     pid = spawn_link __MODULE__, :chain, []
-    send pid, {:continue, 10}
+    send pid, {:continue, chain_size, caller}
     {:ok, pid}
   end
 end
